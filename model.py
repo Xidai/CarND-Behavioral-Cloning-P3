70ea916e341ae4aaf1715f51d4ba3ae4bf68790f
Daixi <xi.dai1990@gmail.com>
2017-02-26 18:07:36 +0800

    use lenet

--- model.py
+++ model.py
@@ -25,9 +25,17 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
+from keras.layers import Convolution2D, MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
