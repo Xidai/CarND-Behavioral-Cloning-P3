ae8e8ec1b1ad0fece89d3f3880db29465e010189
Daixi <xi.dai1990@gmail.com>
2017-02-26 18:40:14 +0800

    add cropping layer

--- model.py
+++ model.py
@@ -32,11 +32,12 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Cropping2D
 from keras.layers import Convolution2D, MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
