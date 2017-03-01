11b30781b8f154ed06b10e4d2e6858fa3d3463fd
xidai <xi.dai1990@gmail.com>
2017-03-01 05:09:18 +0000

    add a dropout layer and train. epoch: 9

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Cropping2D
+from keras.layers import Flatten, Dense, Cropping2D, Dropout
 from keras.layers import Convolution2D, MaxPooling2D, Lambda
 
 model = Sequential()
@@ -47,6 +47,7 @@ model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -54,6 +55,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=9)
 
 model.save('model.h5')
